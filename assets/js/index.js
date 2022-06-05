336a828dde1c7d2085d179a8b918bfd454f669f9
hermitex <mercytibs@yahoo.com>
2022-06-05 15:09:01 +0300

    improve responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,17 @@ import projects from "./projects.js";
 
 // Grab the projects container
 let projectContainer = document.querySelector(".projects");
-let  projectDiv  = '';
+let projectDiv = "";
 
 // Grab the the project background image
-let projectBackgroundImage = document.querySelector('.project-bg-img');
+let projectBackgroundImage = document.querySelector(".project-bg-img");
 
 // Grab the project overlay container
-let projectOverlay = document.querySelector('.project-overlay')
+let projectOverlay = document.querySelector(".project-overlay");
 
 // Prepare the projects page
 projects.forEach((project, i) => {
-    projectDiv += `
+  projectDiv += `
     <div class="project-bg-img" id=project_${i}>
     <div class="project-overlay">
     <h1 class="project-title">${project.name}</h1>
@@ -23,16 +23,15 @@ projects.forEach((project, i) => {
     <a href="${project.url}" class="border-secondary">Visit Website</a>
   </div>
   </div>
-  `; 
- 
+  `;
 });
 
 // Insert the project to the DOM
 projectContainer.innerHTML = projectDiv;
 
 // Attach the projectBackgroundImage with an hover event
-console.log(projectBackgroundImage)
-projectBackgroundImage.addEventListener('click', ()=>{
-    alert('u')
-  projectOverlay.classList.add('make-visible');
-})
+console.log(projectBackgroundImage);
+projectBackgroundImage.addEventListener("click", () => {
+  alert("u");
+  projectOverlay.classList.add("make-visible");
+});
